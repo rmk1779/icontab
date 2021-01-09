3f542edc5fdd8e238d769979cbfbc3752ee4860e
rmk1779 <rrmk17@gmail.com>
2021-01-09 20:10:05 +0530

    Added my project

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,54 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterType"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller, Filter, FilterType) {
+		"use strict";
+
+		return Controller.extend("icontab.controller.App", {
+			onInit: function () {
+
+			},
+
+			onFilterSelect: function(oEvent){
+			
+				let aFilters = [];
+				let sKey = oEvent.getSource().getSelectedKey();
+				let oTable = this.getView().byId("idProductsTable");
+				let oBinding = oTable.getBinding("items");
+
+				if(sKey === "Ok"){
+
+					let stDate = oTable.getItems()[0].getBindingContext().getObject().StartDate;
+
+					let oFilterDate = new Filter("StartDate", "LT", stDate);
+					aFilters.push(oFilterDate);
+
+				}
+				else if(sKey === "Heavy"){
+
+					let stDate = oTable.getItems()[0].getBindingContext().getObject().StartDate;
+
+					let oFilterDate = new Filter("StartDate", "GT", stDate);
+					aFilters.push(oFilterDate);
+
+				}else if(sKey === "Overweight"){
+
+					let stDate = oTable.getItems()[0].getBindingContext().getObject().StartDate;
+
+					let oFilterDate = new Filter("StartDate", "EQ", stDate);
+					aFilters.push(oFilterDate);
+
+				}
+
+				oBinding.filter(aFilters);
+
+				// oTable.bindItems("/EmpDetailsSet", oTemplate, null, aFilters);
+
+			}
+		});
+	});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"icontab/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
